30808d31d3308e9a4b344be73d1084cfc8262354
Juhász Norbert <h870945@stud.u-szeged.hu>
2021-06-08 21:53:31 +0200

    Created controller package and ContactController.java file

---
+++ src/main/java/hu/suaf/demo/controller/ContactController.java
@@ -0,0 +1,14 @@
+package hu.suaf.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+
+    @GetMapping("/contact/create")
+    public String createContactForm() {
+        return "create-contact";
+    }
+}
