50b4b15f5a36895ee424e55eabfec8bcc2b81004
Juhász Norbert <h870945@stud.u-szeged.hu>
2021-06-09 22:12:39 +0200

    Update ContactController.java with setContactSrvice(), listContacts() and createContact() methods

--- src/main/java/hu/suaf/demo/controller/ContactController.java
+++ src/main/java/hu/suaf/demo/controller/ContactController.java
@@ -1,14 +1,38 @@
 package hu.suaf.demo.controller;
 
+import hu.suaf.demo.model.Contact;
+import hu.suaf.demo.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/contact")
 public class ContactController {
 
-    @GetMapping("/contact/create")
+    private ContactService contactService;
+
+    @Autowired
+    public void setContactService(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @GetMapping("/create")
     public String createContactForm() {
-        return "create-contact";
+        return "contact-create";
+    }
+
+    @GetMapping
+    public String lisContacts() {
+        return "contact-list";
+    }
+
+    @PostMapping("/create")
+    public String createContact(Contact c) {
+
+        contactService.addContact(c);
+        return "redirect:/contact";
     }
 }
