2b41b52a037f52a4ab8b655ec59a0489676ebb28
Juhász Norbert <h870945@stud.u-szeged.hu>
2021-06-13 20:45:59 +0200

    Added initBinder to Date type conversation

--- src/main/java/hu/suaf/demo/controller/ContactController.java
+++ src/main/java/hu/suaf/demo/controller/ContactController.java
@@ -3,10 +3,17 @@ package hu.suaf.demo.controller;
 import hu.suaf.demo.model.Contact;
 import hu.suaf.demo.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/contact")
@@ -20,19 +27,25 @@ public class ContactController {
     }
 
     @GetMapping("/create")
-    public String createContactForm() {
+    public String createContactForm(Contact contact) {
         return "contact-create";
     }
 
     @GetMapping
-    public String lisContacts() {
+    public String lisContacts(Model model) {
+        model.addAttribute("contacts", contactService.getContacts());
         return "contact-list";
     }
 
     @PostMapping("/create")
-    public String createContact(Contact c) {
+    public String createContact(Contact contact) {
 
-        contactService.addContact(c);
+        contactService.addContact(contact);
         return "redirect:/contact";
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true));
     }
 }
